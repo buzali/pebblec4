f7cbc5805e99c2e30fb7d67e80d1834686f3c6a1
Tofi Buzali <tofi.buzali@gmail.com>
2014-03-10 18:51:13 -0600

    Missing brackets

--- helloworld.js
+++ helloworld.js
@@ -9,6 +9,7 @@ simply.on('singleClick', function(e) {
     ajax({ url: 'http://192.168.1.230:8080/apaga_tofi' }, function(data){
   var headline = data.match(/<body>(.*?)<P\/>/)[1];
   simply.title(headline);
-  }
+  })
   
+};
 });
