3fb2665ad257f6b43509644776b170a5f8419fea
Tofi Buzali <tofi.buzali@gmail.com>
2014-03-10 18:57:55 -0600

    Empty subtitle after successfull API call

--- helloworld.js
+++ helloworld.js
@@ -1,17 +1,18 @@
-simply.text({ title: 'Control4 on Pebble', subtitle: "Down Key: Turn lights ON\n Left Key: Turn lights OFF" });
+simply.text({ title: 'Control4 on Pebble', subtitle: "Down Key: Turn lights ON\nLeft Key: Turn lights OFF" });
 
 
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
   	ajax({ url: 'http://192.168.1.230:8080/prende_tofi' }, function(data){
-  var headline = data.match(/<body>(.*?)<P\/>/)[1];
-  simply.title("Lights ON!");
+  var response = data.match(/<body>(.*?)<P\/>/)[1];
+  simply.text({ title: 'Lights ON!', subtitle: "" });
+
 });
     
   } else if (e.button === 'down') {
     ajax({ url: 'http://192.168.1.230:8080/apaga_tofi' }, function(data){
-  var headline = data.match(/<body>(.*?)<P\/>/)[1];
-  simply.title("Lights OFF!");
+  var response = data.match(/<body>(.*?)<P\/>/)[1];
+  simply.text({ title: 'Lights OFF!', subtitle: "" });
   })
   
 };
