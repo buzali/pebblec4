1caf4670f3e128bafa60ed525f09a44d605c16ce
Tofi Buzali <tofi.buzali@gmail.com>
2014-03-10 18:49:46 -0600

    Test up down buttons with Control4 API ping

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,14 @@
-simply.vibe('short');
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+  	ajax({ url: 'http://192.168.1.230:8080/prende_tofi' }, function(data){
+  var headline = data.match(/<body>(.*?)<P\/>/)[1];
+  simply.title(headline);
+});
+    
+  } else if (e.button === 'down') {
+    ajax({ url: 'http://192.168.1.230:8080/apaga_tofi' }, function(data){
+  var headline = data.match(/<body>(.*?)<P\/>/)[1];
+  simply.title(headline);
+  }
+  
+});
