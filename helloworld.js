0ec5dee08de7aca7705cfcbea59ee151341851eb
Tofi Buzali <tofi.buzali@gmail.com>
2014-03-10 18:55:55 -0600

    Add main screen text

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,6 @@
+simply.text({ title: 'Control4 on Pebble', subtitle: "Down Key: Turn lights ON\n Left Key: Turn lights OFF" });
+
+
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
   	ajax({ url: 'http://192.168.1.230:8080/prende_tofi' }, function(data){
