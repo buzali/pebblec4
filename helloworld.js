9c83ae51a938ede644a5c6159a3c86f15e2949d6
Tofi Buzali <tofi.buzali@gmail.com>
2014-03-10 18:53:41 -0600

    Add strings after successful API calls

--- helloworld.js
+++ helloworld.js
@@ -2,13 +2,13 @@ simply.on('singleClick', function(e) {
   if (e.button === 'up') {
   	ajax({ url: 'http://192.168.1.230:8080/prende_tofi' }, function(data){
   var headline = data.match(/<body>(.*?)<P\/>/)[1];
-  simply.title(headline);
+  simply.title("Lights ON!");
 });
     
   } else if (e.button === 'down') {
     ajax({ url: 'http://192.168.1.230:8080/apaga_tofi' }, function(data){
   var headline = data.match(/<body>(.*?)<P\/>/)[1];
-  simply.title(headline);
+  simply.title("Lights OFF!");
   })
   
 };
